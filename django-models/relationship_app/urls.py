60437753cd8699a42d0b161fdccf0853f0d9e980
Mallifs <mallifs@icloud.com>
2024-08-18 08:23:27 +0300

    Intro to Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from .views import book_list, LibraryDetailView
 from django.contrib.auth import views as auth_views
 from .views import RegisterView
-from django.contrib import admin
-from .views import admin_view
-from .views import librarian_view
-from .views import member_view
-
- 
 
 urlpatterns = [
     path('books/', book_list, name='book-list'),
@@ -15,8 +9,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('admin/', admin.site.urls),
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,29 +4,6 @@ from .models import Book, Library
 from django.views.generic import DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
-
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_view.html')
-
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
 
 class RegisterView(CreateView):
     form_class = UserCreationForm

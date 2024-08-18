16705bc6b9ada49f67bb6848d8d7dfa77293ba6c
Mallifs <mallifs@icloud.com>
2024-08-18 23:52:35 +0300

    Intro to Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView
 from django.contrib.auth import login
+from .views import list_books
 
 
 

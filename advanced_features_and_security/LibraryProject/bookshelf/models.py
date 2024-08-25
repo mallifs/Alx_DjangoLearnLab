64d44edd6f2d930b5d641f1e310ec08fec2597bf
Mallifs <mallifs@icloud.com>
2024-08-25 06:32:39 +0300

    advanced Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length= 200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField(default=1880)
+
+    def __str__(self):
+        return self.title
+
+class Meta:
+        permissions = [
+            ("can_add_book", "add book"),
+            ("can_change_book", "change book"),
+            ("can_delete_book", "delete book"),
+        ]    

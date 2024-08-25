82d81d9748ca5211901d3c2d8f45afcade637767
Mallifs <mallifs@icloud.com>
2024-08-25 07:01:01 +0300

    advanced Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from .models import CustomUser
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length= 100)
+    publication_year = models. IntegerField()
+     
+def __str__(self):
+        return self.title
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create a book"),
+            ("can_delete", "Can delete a book"),
+        ]
+
+    def __str__(self):
+        return self.title
+    
 
 
 class CustomUserManager(BaseUserManager):
@@ -27,18 +57,3 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-
-class Book(models.Model):
-    title = models.CharField(max_length= 200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField(default=1880)
-
-    def __str__(self):
-        return self.title
-
-class Meta:
-        permissions = [
-            ("can_add_book", "add book"),
-            ("can_change_book", "change book"),
-            ("can_delete_book", "delete book"),
-        ]    
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,38 +1,76 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Book
 from .form import BookForm
+from django.http import HttpResponse
+from .form import ExampleForm
 
-@permission_required('relationship_app.add_book', raise_exception=True)
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')  # Redirect to a list or detail view
-    else:
-        form = BookForm()
-    return render(request, 'add_book.html', {'form': form})
-
-@permission_required('relationship_app.change_book', raise_exception=True)
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+# Function-Based View for Editing a Book
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    
     if request.method == 'POST':
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_detail', pk=book.pk)
+            return redirect('book_list')
     else:
         form = BookForm(instance=book)
-    return render(request, 'edit_book.html', {'form': form, 'book': book})
+    
+    return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
-@permission_required('relationship_app.delete_book', raise_exception=True)
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-    return render(request, 'confirm_delete.html', {'book': book})
+# Function-Based View for Deleting a Book
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return redirect('book_list')
 
+# Class-Based View for Creating a Book
+class BookCreateView(PermissionRequiredMixin, CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'bookshelf/add_book.html'
+    permission_required = 'bookshelf.can_add'
+    raise_exception = True
 
+    def form_valid(self, form):
+        form.instance.added_by = self.request.user
+        return super().form_valid(form)
+
+# Class-Based View for Editing a Book
+class BookUpdateView(PermissionRequiredMixin, UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'bookshelf/edit_book.html'
+    permission_required = 'bookshelf.can_edit'
+    raise_exception = True
+
+# Class-Based View for Deleting a Book
+class BookDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Book
+    template_name = 'bookshelf/confirm_delete.html'
+    success_url = reverse_lazy('book_list')
+    permission_required = 'bookshelf.can_delete'
+    raise_exception = True
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_list(request):
+    return HttpResponse
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_list(request):
+    return HttpResponse
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_list(request):
+    return HttpResponse
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    return HttpResponse

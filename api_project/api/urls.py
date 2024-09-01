b54c673e09df1f476dcb4fe6b66a354611d01340
Mallifs <mallifs@icloud.com>
2024-09-01 17:25:45 +0300

    Django-api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,29 +1,12 @@
-from api.urls import path
-from .views import BookList
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import BookViewSet
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-]
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include("api.urls")),  # Include API app URLs
-]
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'book', BookViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
 
-urlpatterns = [
-    path('books/', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+urlpatterns =[
+    
+    path('', BookViewSet.as_view('Book'), name = 'book_list',)
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework import viewsets
-from api.models import Book
+from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
-
-class BookList(generics.ListAPIView):
-        queryset = Book.objects.all()
-        serializer_class = BookSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookViewSet(viewsets.ModelViewSet):
-        queryset = Book.objects.all()
-        serializer_class = BookSerializer
-
-
-class BookViewSet(viewsets.ModelViewSet):
-        queryset = Book.objects.all()
-        serializer_class = BookSerializer
-        permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]

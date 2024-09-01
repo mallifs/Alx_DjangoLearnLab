87ffb2741ac0e0ccc878444a4343890dd749c206
Mallifs <mallifs@icloud.com>
2024-09-01 17:16:48 +0300

    Django-api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -12,7 +12,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include("api.urls")),
+    path('api/', include(["api.urls"])),
     path('admin/', admin.site.urls)
   
 ]

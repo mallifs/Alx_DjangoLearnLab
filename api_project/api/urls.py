fc5b2ea7a9b3a6d62e2e259daa810cf7a2a91a66
Mallifs <mallifs@icloud.com>
2024-09-01 17:31:02 +0300

    Django-api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,29 @@
-from django.urls import path
-from .views import BookViewSet
+from api.urls import path
+from .views import BookList
 from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include("api.urls")),  # Include API app URLs
+]
 
 router = DefaultRouter()
-router.register(r'book', BookViewSet)
+router.register(r'books', BookViewSet)
 
+urlpatterns = [
+    path('', include(router.urls)),
+]
 
-urlpatterns =[
-    
-    path('', BookViewSet.as_view('Book'), name = 'book_list',)
+urlpatterns = [
+    path('books/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from api.models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+
+    class BookViewSet(viewsets.ModelViewSet):
+        queryset = Book.objects.all()
+        serializer_class = BookSerializer
+        permission_classes = [IsAuthenticated]  # Require authentication

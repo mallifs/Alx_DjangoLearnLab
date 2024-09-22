cfdd5b937e455ad633b264d6b40c42feef2dba6f
Mallifs <mallifs@icloud.com>
2024-09-22 14:28:09 +0300

    project-setup

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.response import Response
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
